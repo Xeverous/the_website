0403b4471c7a4f8ca5a49ce9ecbe7f8b0cd2a2e4
Xeverous <20820409+Xeverous@users.noreply.github.com>
2020-05-17 20:57:36 +0200

    index plugin - index generation

--- website/pages/index/index.md
+++ website/pages/index/index.md
@@ -4,6 +4,7 @@
 .. date: 2019-08-15 16:27:52 UTC+02:00
 .. tags:
 .. category:
+.. link:
 .. description: index of all pages on the website
 .. type: text
 .. author: Xeverous
--- website/plugins/generate_index.py
+++ website/plugins/generate_index.py
@@ -29,29 +29,52 @@ class IndexGenerationShortcode(ShortcodePlugin):
         Ouput: a HTML string (nested <ul> elements containing <a> elements)
         and a list of file dependencies.
         """
-        if not post.metadata.is_index_page:
-            raise RuntimeError(f'Attempt to generate index on "{post.permalink()}" which is not an index page!')
+        # site.pages: List[Post]
+        root_dir: PageDir = make_tree(site.pages)
+        html_result = generate_hierarchical_html(root_dir)
 
-        # index pages can list contents of arbitrary directory
-        # find first which directory the page refers to
-        current_dir: PageDir = site.metadata.structure
-        index_path: str = post.meta[post.default_lang]["index_path"]
-        if index_path == ".":
-            for directory_name in directories_from_path(post.permalink()):
-                current_dir = current_dir.enter(directory_name)
-        elif index_path != "/":
-            for directory_name in directories_from_path(index_path):
-                current_dir = current_dir.enter(directory_name)
+        return html_result, list()
 
-        html_result: str = generate_hierarchical_html(current_dir)
 
-        # We need to regenerate indexes every time a page is added or removed.
-        # We can not return wildcard paths or generally - paths which do not exist
-        # but there is a hidden (undocumented) feature that does exactly this.
-        # https://github.com/getnikola/nikola/issues/3293#issuecomment-523210046
-        file_dependencies = ["####MAGIC####TIMELINE"]
+class PageDir:
+    def __init__(self, directory: str, pages: List[Post] = None, subdirs: List[PageDir] = None):
+        self.directory = directory
+        self.pages = pages or []
+        self.subdirs = subdirs or []
 
-        return html_result, file_dependencies
+    def traverse(self, directory: str) -> PageDir:
+        """Return a nested directory object. If it does not exist, create one and then return it."""
+        for subdir in self.subdirs:
+            if subdir.directory == directory:
+                return subdir
+
+        self.subdirs.append(PageDir(directory))
+        return self.subdirs[-1]
+
+
+def directories_from_path(path: str) -> List[str]:
+    """
+    Strip slashes from the path and split the resulting directories into a list
+
+    Example: "/foo/bar/baz/quux/" => ["foo", "bar", "baz", "quux"]
+    """
+    return path.strip("/").split("/")
+
+
+def make_tree(pages: List[Post]) -> PageDir:
+    root_dir = PageDir("/")
+    for page in pages:
+        current = root_dir
+        for directory in directories_from_path(page.permalink()):
+            current = current.traverse(directory)
+        current.pages.append(page)
+
+    return root_dir
+
+
+def make_li_element(page: Post) -> str:
+    """Make a HTML element: <li><a href="path">text</a></li>"""
+    return '<li><a href="{0}">{1}</a></li>'.format(page.permalink(), page.title())
 
 
 def generate_hierarchical_html(root_dir: PageDir) -> str:
@@ -65,14 +88,16 @@ def generate_hierarchical_html(root_dir: PageDir) -> str:
 
 
 def generate_hierarchical_html_impl(root_dir: PageDir, html_parts: List[str]) -> None:
+    if not root_dir.pages and not root_dir.subdirs:
+        return
+
     html_parts.append("<ul>")
+    html_parts.append(root_dir.directory)
 
     for page in root_dir.pages:
-        html_parts.append(f'<li><a href="{page.permalink()}">{page.title()}</a></li>')
+        html_parts.append(make_li_element(page))
 
     for subdir in root_dir.subdirs:
-        html_parts.append(f'<li><a href="{subdir.index_page.permalink()}">{subdir.index_page.title()}</a>')
         generate_hierarchical_html_impl(subdir, html_parts)
-        html_parts.append("</li>")
 
     html_parts.append("</ul>")
