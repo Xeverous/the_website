7dcce2159894a2f7564f25b32e73e2cb59fa0b59
Xeverous <20820409+Xeverous@users.noreply.github.com>
2022-05-03 15:52:03 +0200

    CSS - fmt class for format highlight

--- website/plugins/rest_highlighter.py
+++ website/plugins/rest_highlighter.py
@@ -87,7 +87,7 @@ def read_file_split_lines(path: str) -> List[str]:
 # CCH implementation
 ##############################################################################
 
-VALID_CSS_CLASSES = ("keyword;type;namespace;enum;ext;func;oo;pref;suf;label;spec;"
+VALID_CSS_CLASSES = ("keyword;type;namespace;enum;ext;func;oo;pref;suf;fmt;label;spec;"
 "var_local;var_member;var_global;param;outparam;tparam;concept;problem;num;str;str_esc;chr;chr_esc;"
 "com_single;com_multi;com_tag;com_single_dox;com_multi_dox;com_tag_dox;pp_direct;pp_header;pp_other;"
 "macro_def;macro_ref;macro_param;macro_body;asm_direct")
